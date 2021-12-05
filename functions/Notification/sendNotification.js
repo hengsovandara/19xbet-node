1bc0aec26e097171a7d6a8c4eb4cd6ddf2315eeb
HengSovandara <hengsovandara15@gmail.com>
2021-12-05 15:45:02 +0700

    update

--- functions/Notification/sendNotification.js
+++ functions/Notification/sendNotification.js
@@ -1,19 +1,28 @@
 const { parseEvent } = require('../utils/helpers')
 const { success, fail, getRequestAct } = require('hasu')
-const { BOT_TOKEN, CHAT_ID  } = require('../../config')
+const { BOT_TOKEN, REGISTER_CHAT_ID, DEPOSIT_CHAT_ID, WITHDRAW_CHAT_ID  } = require('../../config')
 
 module.exports.handler = async event => {
   try {
     const { body: { event: { data: { new: newData } }, table: { name }} } = parseEvent(event)
-    let url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${CHAT_ID}`
+    let url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?`
     let text = ''
+    let chatId = ''
     switch (name) {
       case 'Users':
         text = "*អតិថិជនចុះឈ្មោះ*\n\n" + "ឈ្មោះ: " + newData.name + "\n\n" + "លេខទូរស័ព្ទ: 0" + newData.phoneNumber + "\n\n" + "-----------------------------------------------------"
+        chatId = REGISTER_CHAT_ID
         break;
       default:
         const { user } = await getUserInfo(newData.userId)
-        const type = newData.type === "cashOut" ? "ដកប្រាក់" : "ដាក់ប្រាក់"
+        let type = ''
+        if(newData.type === "cashOut"){
+          chatId = WITHDRAW_CHAT_ID
+          type = "ដកប្រាក់"
+        }else{
+          chatId = DEPOSIT_CHAT_ID
+          type = "ដាក់ប្រាក់"
+        }
         text = "-------------------------------------------------" + "\n\n" + 
         "អតិថិជន" + type + "\n\n" + 
         "លេខ: " + newData.index + "\n\n" + 
@@ -25,9 +34,11 @@ module.exports.handler = async event => {
         break;
     }
         
-    url += `&text=${encodeURI(text)}&parse_mode=Markdown`
+    // url += `chat_id=-${chatId}&text=${encodeURI(text)}&parse_mode=Markdown`
+    url += `chat_id=-${chatId}`
 
-    const newResult =  await getRequestAct("GET", {url}).then(res => res).catch(error => {throw error})
+    const newResult =  await getRequestAct("POST", {url, body: { text }}).then(res => res).catch(error => {throw error})
+    // const newResult =  await getRequestAct("GET", {url}).then(res => res).catch(error => {throw error})
 
     return success(newResult)
   } catch (error) {
